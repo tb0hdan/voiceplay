a42f1225896f0ec7614809c8706eb3783ae67f91
Bohdan Turkynewych <tb0hdan@gmail.com>
2016-07-31 20:16:29 +0300

    wakeword listener

--- voiceplay.sh
+++ voiceplay.sh
@@ -2,6 +2,8 @@
 
 if [ "$1" == "requirements" ] || [ "$1" == "requirements.txt" ]; then
     sudo pip install -r requirements.txt
+elif [ "$1" == "wakeword" ]; then
+    PYTHONPATH=./ python -m voiceplay.recognition.wakeword.sender
 else
     PYTHONPATH=./ python -m voiceplay.cli.main $@
 fi
