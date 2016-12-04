aed823517de103afd944ba7194362f5800a53ced
Bohdan Turkynewych <tb0hdan@gmail.com>
2016-12-04 21:08:56 +0200

    v0.2.0 bump

--- voiceplay/__init__.py
+++ voiceplay/__init__.py
@@ -2,7 +2,7 @@
 ''' get version, etc '''
 
 __title__ = 'VoicePlay'
-__version__ = '0.1.9'
+__version__ = '0.2.0'
 __author__ = 'Bohdan Turkynewych'
 __license__ = 'UNLICENSE'
 __copyright__ = 'public domain'
