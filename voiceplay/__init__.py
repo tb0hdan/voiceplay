8b4915254e07377b5b0018a2337e997a6cb9d97e
Bohdan Turkynewych <tb0hdan@gmail.com>
2016-12-09 01:11:51 +0200

    vars

--- voiceplay/__init__.py
+++ voiceplay/__init__.py
@@ -2,6 +2,7 @@
 ''' get version, etc '''
 
 __title__ = 'VoicePlay'
+__description__  = 'Client-side first music centered voice controlled player'
 __version__ = '0.2.0'
 __author__ = 'Bohdan Turkynewych'
 __license__ = 'UNLICENSE'
