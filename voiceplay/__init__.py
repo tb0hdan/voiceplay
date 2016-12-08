cf04d26c34490967c10c9b91434d5d4ea1c85d94
Bohdan Turkynewych <tb0hdan@gmail.com>
2016-12-09 01:14:18 +0200

    dev version bump

--- voiceplay/__init__.py
+++ voiceplay/__init__.py
@@ -3,7 +3,7 @@
 
 __title__ = 'VoicePlay'
 __description__  = 'Client-side first music centered voice controlled player'
-__version__ = '0.2.0'
+__version__ = '0.2.1'
 __author__ = 'Bohdan Turkynewych'
 __license__ = 'UNLICENSE'
 __copyright__ = 'public domain'
